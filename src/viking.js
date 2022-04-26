fae93de7084589a8d224436ccd835b911226bfa8
Daniel Gonzalez <danielgnzch@gmail.com>
2022-04-26 17:16:42 +0200

    Add iteration 1 & 2

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,76 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    
+    this.health = health;
+    this.strength = strength;
+  }
+
+  attack(){
+    return this.strength;
+  }
+
+  receiveDamage(damage){
+    this.health = this.health - damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+  constructor(name, health, strength) {
+
+    super(health, strength);    
+    this.name = name;    
+  }
+
+  receiveDamage(damage){
+
+    this.health = this.health - damage;
+
+    if(this.health > 0){
+      return `${this.name} has received damage points of ${damage}`;
+    }else{
+      return `${this.name} has died in act of combat`;
+    }
+
+  }
+
+  battleCry(){
+     return "Odin Owns You All!";
+  }
+}
 
 // Saxon
 class Saxon {}
 
+
+
+
 // War
 class War {}
+
+
+let soldier;
+const strength = 150;
+const health = 300;
+
+soldierManolo = new Soldier(health, strength);
+
+console.log(soldierManolo.attack());
+
+
+let viking;
+const name = 'Harald';
+const strength2 = 160;
+const health2 = 310;
+
+vikingLauri = new Viking(name, health2, strength2);
+console.log(vikingLauri.attack());
+console.log(vikingLauri.receiveDamage(150));
+console.log(vikingLauri.receiveDamage(150));
+console.log(vikingLauri.receiveDamage(150));
+console.log(vikingLauri.health);
+console.log(vikingLauri.battleCry());
 
 
 
