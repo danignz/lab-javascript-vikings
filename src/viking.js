241b439ecc29f7b0edf840b727bbcd7b8726619d
Daniel Gonzalez <danielgnzch@gmail.com>
2022-04-26 17:47:03 +0200

    Add iteration 3

--- src/viking.js
+++ src/viking.js
@@ -1,7 +1,6 @@
 // Soldier
 class Soldier {
   constructor(health, strength) {
-    
     this.health = health;
     this.strength = strength;
   }
@@ -18,21 +17,17 @@ class Soldier {
 // Viking
 class Viking extends Soldier{
   constructor(name, health, strength) {
-
     super(health, strength);    
     this.name = name;    
   }
 
   receiveDamage(damage){
-
     this.health = this.health - damage;
-
     if(this.health > 0){
       return `${this.name} has received damage points of ${damage}`;
     }else{
       return `${this.name} has died in act of combat`;
     }
-
   }
 
   battleCry(){
@@ -41,36 +36,51 @@ class Viking extends Soldier{
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
 
-
-
+  receiveDamage(damage){
+    this.health = this.health - damage;
+    if(this.health > 0){
+      return `A Saxon has received ${damage} points of damage`;
+    }else{
+      return `A Saxon has died in combat`;
+    }
+  }
+}
 
 // War
 class War {}
 
 
-let soldier;
+
 const strength = 150;
 const health = 300;
-
-soldierManolo = new Soldier(health, strength);
-
+const soldierManolo = new Soldier(health, strength);
 console.log(soldierManolo.attack());
 
 
-let viking;
+
 const name = 'Harald';
 const strength2 = 160;
 const health2 = 310;
-
-vikingLauri = new Viking(name, health2, strength2);
+const vikingLauri = new Viking(name, health2, strength2);
 console.log(vikingLauri.attack());
 console.log(vikingLauri.receiveDamage(150));
 console.log(vikingLauri.receiveDamage(150));
 console.log(vikingLauri.receiveDamage(150));
 console.log(vikingLauri.health);
 console.log(vikingLauri.battleCry());
+
+
+
+const health3 = 60;
+const strength3 = 25;
+const saxonMikel = new Saxon(health3, strength3);
+console.log(saxonMikel.attack());
+console.log(saxonMikel.health);
+console.log(saxonMikel.receiveDamage(50));
+console.log(saxonMikel.receiveDamage(150));
+console.log(saxonMikel.health);
 
 
 
